d82423948b0f8ab7c14deff5091c901cafd2bd59
Yenni Vanessa Delgado <yennivanessadelgado@gmail.com>
2022-08-16 14:32:33 -0500

    Este es el primer ejercicio de arreglos

---
+++ NetBeansProjects/Arreglo1/src/arreglo1/Arreglo1.java
@@ -0,0 +1,19 @@
+
+package arreglo1;
+
+
+public class Arreglo1 {
+
+
+    public static void main(String[] args) {
+        
+    String productos []={"Vestidos,Zapatos,Blusas"};
+    double precios[]= {35.0,25.0,10.0,40.0};
+    
+        
+        for (int i= 0; i< productos.length; i++){
+            System.out.println(productos [i]+"="+ precios [i]);
+    }
+    }
+    
+}
